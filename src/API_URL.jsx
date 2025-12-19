938a0e9c11de4e43e324fd3c627476dea9fb4fe5
shubham <shubhamlowanshi12345@gmail.com>
2025-12-19 17:58:24 +0530

    comment

--- src/API_URL.jsx
+++ src/API_URL.jsx
@@ -1,9 +1,9 @@
-export const    __userapiurl="https://sheepping-backend.onrender.com/user/";
+export const    __userapiurl="http://localhost:3001/user/";
 
-export const __categoryapiurl="https://sheepping-backend.onrender.com/category/";
+export const __categoryapiurl="http://localhost:3001/category/";
 
-export const __subcategoryapiurl="https://sheepping-backend.onrender.com/subcategory/";
+export const __subcategoryapiurl="http://localhost:3001/subcategory/";
 
-export const __shipmentapiurl="https://sheepping-backend.onrender.com/shipment/"
+export const __shipmentapiurl="http://localhost:3001/shipment/"
 
-export const __bidapiurl="https://sheepping-backend.onrender.com/bid/"
+export const __bidapiurl="http://localhost:3001/bid/"
